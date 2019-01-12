c535ad6eed74144bd18a6efd386d36cba4ac190b
moustachu <git@moustachu.net>
2019-01-13 00:31:11 +0100

    :sparkles: Committee & Services roles
    - add new global config accessor `:participatory_process_user_roles`
    - **overwrite** `Decidim::ParticipatoryProcessUserRole` to use new 
    config
    - add 2 new roles "committee" & "service"
    - basic i18n
    - basic test suite

--- config/initializers/extends.rb
+++ config/initializers/extends.rb
@@ -1,3 +1,4 @@
+require 'extends/decidim-core/core_extend.rb'
 require 'extends/decidim-core/commands/decidim/amendable/create_extend.rb'
 require 'extends/decidim-core/commands/decidim/amendable/promote_extend.rb'
 require 'extends/decidim-core/helpers/decidim/cells_helper_extend.rb'
