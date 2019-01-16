3ef29e0aecc2649fc886975aeb0acc54735fc236
moustachu <git@moustachu.net>
2019-01-16 10:51:06 +0100

    :green_heart: :white_check_mark: fix "Models Extends" vs "Old DB Migrations"

--- config/initializers/extends.rb
+++ config/initializers/extends.rb
@@ -1,7 +1,26 @@
 require 'extends/decidim-core/core_extend.rb'
 require 'extends/decidim-core/helpers/decidim/application_helper_extend.rb'
 require 'extends/decidim-admin/helpers/decidim/admin/application_helper_extend.rb'
-require 'extends/decidim-participatory_processes/models/decidim/participatory_process_extend.rb'
+
+ActiveSupport.on_load(:active_record) {
+
+  def database_exists?
+    ActiveRecord::Base.connection
+  rescue ActiveRecord::NoDatabaseError
+    false
+  else
+    true
+  end
+
+  # Models needs to be loaded AFTER DB init because of legacy Migrations
+  if database_exists?
+    if ActiveRecord::Base.connection.table_exists?(:decidim_participatory_processes)
+      require 'extends/decidim-participatory_processes/models/decidim/participatory_process_extend.rb'
+    end
+  end
+  
+}
+
 require 'extends/decidim-participatory_processes/controllers/decidim/participatory_processes/admin/participatory_processes_controller_extend.rb'
 require 'extends/decidim-core/commands/decidim/amendable/create_extend.rb'
 require 'extends/decidim-core/commands/decidim/amendable/promote_extend.rb'
