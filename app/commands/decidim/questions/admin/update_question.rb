6e4c0222ee1416f519dcd0194a3386b9be14c4a1
Armand <fardeauarmand@gmail.com>
2019-01-23 22:55:46 +0100

    Add update without versionning method

--- app/commands/decidim/questions/admin/update_question.rb
+++ app/commands/decidim/questions/admin/update_question.rb
@@ -47,18 +47,45 @@ module Decidim
         attr_reader :form, :question, :attachment
 
         def update_question
+          return update_without_versioning if only_recipient_or_state_changed?
+
+          update_with_versioning
+        end
+
+        def update_with_versioning
           Decidim.traceability.update!(
-              question,
-              form.current_user,
-              title: title_with_hashtags,
-              body: body_with_hashtags,
-              category: form.category,
-              scope: form.scope,
-              address: form.address,
-              latitude: form.latitude,
-              longitude: form.longitude,
-              created_in_meeting: form.created_in_meeting,
-              recipient: form.recipient
+            question,
+            form.current_user,
+            title: title_with_hashtags,
+            body: body_with_hashtags,
+            category: form.category,
+            scope: form.scope,
+            address: form.address,
+            latitude: form.latitude,
+            longitude: form.longitude,
+            created_in_meeting: form.created_in_meeting,
+            recipient: form.recipient,
+            state: form.state
+          )
+        end
+
+        def update_without_versioning
+          PaperTrail.request(enabled: false) do
+            @question.update!(
+              recipient: form.recipient,
+              state: form.state
+            )
+          end
+        end
+
+        def only_recipient_or_state_changed?
+          # TODO: FIND A MORE ELEGANT WAY
+          form.attributes[:title] == question.attributes["title"] &&
+            form.attributes[:body] == question.attributes["body"] &&
+            form.attributes[:category] == question.attributes["category"] &&
+            (
+            form.attributes[:state] != question.attributes["state"] ||
+                form.attributes[:recipient] != question.attributes["recipient"]
           )
         end
       end
