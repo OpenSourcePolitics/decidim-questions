268f19b030d9ee79f3580ecdc939d36612b74f05
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2019-05-16 12:52:50 +0200

    Don't inherit

--- app/forms/decidim/questions/admin/questions_move_form.rb
+++ app/forms/decidim/questions/admin/questions_move_form.rb
@@ -5,9 +5,10 @@ module Decidim
     module Admin
       # A form object to be used when admin users wants to merge two or more
       # questions into a new one to another question component in the same space.
-      class QuestionsMoveForm < QuestionsForkForm
+      class QuestionsMoveForm < Decidim::Form
         validates :questions, length: { minimum: 1 }
         attribute :target_component_id, Integer
+        attribute :question_ids, Array
 
         validates :target_component, :questions, :current_component, presence: true
         validate :same_participatory_space
@@ -17,7 +18,7 @@ module Decidim
         end
 
         def target_component
-          return current_component if clean_target_component_id == current_component.id
+          return current_component if target_component_id.first.to_i == current_component.id
           @target_component ||= current_component.siblings.find_by(id: target_component_id)
         end
       end
