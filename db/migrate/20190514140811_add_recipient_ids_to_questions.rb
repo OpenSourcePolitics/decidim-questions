b96061e87b832a58ea8bff025195a04be7029d57
Oriol Gual <oriol@codegram.com>
2019-05-17 17:41:10 +0200

    Add migration to migrate existing questions

--- db/migrate/20190514140811_add_recipient_ids_to_questions.rb
+++ db/migrate/20190514140811_add_recipient_ids_to_questions.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddRecipientIdsToQuestions < ActiveRecord::Migration[5.2]
   def change
     add_column :decidim_questions_questions, :recipient_ids, :integer, array: true, default: [], null: false
