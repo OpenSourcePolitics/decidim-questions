5050d1a75bc029dc6b1feb928384bae03ebb67ae
moustachu <git@moustachu.net>
2019-06-05 00:15:49 +0200

    Refactor admin permissions #51

--- app/permissions/decidim/questions/admin/permissions.rb
+++ app/permissions/decidim/questions/admin/permissions.rb
@@ -13,35 +13,17 @@ module Decidim
           # The public part needs to be implemented yet
           return permission_action if permission_action.scope != :admin
 
-          has_access? if permission_action.subject == :component && permission_action.action == :read
-          has_access? if permission_action.subject == :question && permission_action.action == :read
-
-          if user.admin? || process_admin?
-            create_actions
-            # Admins can only edit official questions if they are within the
-            # time limit.
-            allow! if permission_action.subject == :question && permission_action.action == :edit
-            allow! if permission_action.subject == :questions && permission_action.action == :move
-            # Every user allowed by the space can update the category of the question
-            allow! if permission_action.subject == :question_category && permission_action.action == :update
-            # Every user allowed by the space can import questions from another_component
-            allow! if permission_action.subject == :questions && permission_action.action == :import
-            # Every user allowed by the space can merge questions to another component
-            allow! if permission_action.subject == :questions && permission_action.action == :merge
-            # Every user allowed by the space can split questions to another component
-            allow! if permission_action.subject == :questions && permission_action.action == :split
-            if permission_action.subject == :participatory_texts && participatory_texts_are_enabled?
-              # Every user allowed by the space can manage (import, update and publish) participatory texts to questions
-              allow! if permission_action.action == :manage
-            end
+          if user.admin? || can_manage_question?(role: :admin)
+            process_admin_action?
+          elsif can_manage_question?(role: :committee)
+            committee_action?
+          elsif can_manage_question?(role: :service)
+            service_action?
+          elsif can_manage_question?(role: :moderator)
+            moderator_action?
+          elsif can_manage_question?(role: :collaborator)
+            collaborator_action?
           end
-
-          committee_action?
-          service_action?
-
-          moderator_action?
-          collaborator_action?
-          process_admin_action?
 
           Rails.logger.debug permission_action.inspect
           Rails.logger.debug "==========="
@@ -51,95 +33,101 @@ module Decidim
 
         private
 
-        def create_actions
-          if create_permission_action?
-            create_note
-            create_question
-            create_question_answer
+        def process_admin_action?
+          # return unless can_manage_question?(role: :admin)
+
+          allow! if permission_action.subject == :component && permission_action.action == :read
+          allow! if permission_action.subject == :component && permission_action.action == :manage
+
+          allow! if permission_action.subject == :question && permission_action.action == :read
+          allow! if permission_action.subject == :question && permission_action.action == :edit
+
+          allow! if permission_action.subject == :questions && permission_action.action == :read
+          allow! if permission_action.subject == :questions && permission_action.action == :move
+          allow! if permission_action.subject == :questions && permission_action.action == :import
+          allow! if permission_action.subject == :questions && permission_action.action == :merge
+          allow! if permission_action.subject == :questions && permission_action.action == :split
+
+          allow! if permission_action.subject == :question_category && permission_action.action == :update
+
+          allow! if permission_action.subject == :question_note && permission_action.action == :create
+
+          if admin_creation_is_enabled?
+            allow! if permission_action.subject == :question && permission_action.action == :create
+          end
+
+          if admin_question_answering_is_enabled?
+            allow! if permission_action.subject == :question && permission_action.action == :answer
+            allow! if permission_action.subject == :question_answer && permission_action.action == :create
+          end
+
+          if permission_action.subject == :participatory_texts && participatory_texts_are_enabled?
+            allow! if permission_action.action == :manage
+          end
+
+        end
+
+        def committee_action?
+          # return unless can_manage_question?(role: :committee)
+
+          allow! if permission_action.subject == :component && permission_action.action == :read
+          allow! if permission_action.subject == :component && permission_action.action == :manage
+
+          allow! if permission_action.subject == :question && permission_action.action == :read
+          allow! if permission_action.subject == :questions && permission_action.action == :read
+
+          allow! if permission_action.subject == :question_note && permission_action.action == :create
+
+          if admin_question_answering_is_enabled? && is_recipient?
+            allow! if permission_action.subject == :question && permission_action.action == :answer
+            allow! if permission_action.subject == :question_answer && permission_action.action == :create
           end
         end
 
-        def create_note
-          # There's no special condition to create question notes, only
-          # users with access to the admin section can do it.
-          allow! if permission_action.subject == :question_note
-        end
-
-        def create_question
-          # Questions can only be created from the admin when the
-          # corresponding setting is enabled.
-          toggle_allow(admin_creation_is_enabled?) if permission_action.subject == :question
-        end
-
-        def create_question_answer
-          # Questions can only be answered from the admin when the
-          # corresponding setting is enabled.
-          toggle_allow(admin_question_answering_is_enabled?) if permission_action.subject == :question_answer
-        end
-
-        def process_admin_action?
-          return unless can_manage_question?(role: :admin)
+        def service_action?
+          # return unless can_manage_question?(role: :service)
 
           allow! if permission_action.subject == :component && permission_action.action == :read
-          allow! if permission_action.subject == :question && permission_action.action == :edit
-          question_actions
+          allow! if permission_action.subject == :component && permission_action.action == :manage
+
+          allow! if permission_action.subject == :question_note && permission_action.action == :create
+
+          if is_recipient?
+
+            allow! if permission_action.subject == :question && permission_action.action == :read
+            allow! if permission_action.subject == :questions && permission_action.action == :read
+
+            if admin_question_answering_is_enabled?
+              allow! if permission_action.subject == :question && permission_action.action == :answer
+              allow! if permission_action.subject == :question_answer && permission_action.action == :create
+            end
+          end
         end
 
         # A moderator needs to be able to read the question component they are assigned to,
         # and needs to perform all actions for the moderations of the question component.
         def moderator_action?
-          return unless can_manage_question?(role: :moderator)
+          # return unless can_manage_question?(role: :moderator)
 
-          create_actions
+          allow! if permission_action.subject == :component && permission_action.action == :read
+
+          allow! if permission_action.subject == :question && permission_action.action == :read
+
+          if question.try(:state).blank?
+            allow! if permission_action.subject == :question && permission_action.action == :edit
+          end
+
+          allow! if permission_action.subject == :questions && permission_action.action == :read
+          allow! if permission_action.subject == :questions && permission_action.action == :move
+
           allow! if permission_action.subject == :moderation
         end
 
         # Collaborators can read/preview everything inside their question component.
         def collaborator_action?
-          return unless can_manage_question?(role: :collaborator)
+          # return unless can_manage_question?(role: :collaborator)
 
-          create_actions
           allow! if permission_action.action == :read || permission_action.action == :preview
-        end
-
-        def process_admin?
-          can_manage_question?(role: :admin)
-
-          create_actions
-        end
-
-        def committee_action?
-          return unless can_manage_question?(role: :committee)
-
-          disallow! if permission_action.subject == :question && permission_action.action == :create
-          create_note
-          create_question_answer
-          question_actions
-        end
-
-        def service_action?
-          return unless can_manage_question?(role: :service)
-
-          create_note
-          create_question_answer
-          question_actions
-        end
-
-        def question_actions
-          allow! if permission_action.subject == :questions && permission_action.action == :read
-          question_note_action
-          question_answer_action
-        end
-
-        def question_note_action
-          # TODO: Use only read action and move component permission to the right place
-          allow! if permission_action.subject == :component && permission_action.action == :manage
-
-          allow! if permission_action.subject == :question_note && permission_action.action == :create
-        end
-
-        def question_answer_action
-          allow! if admin_question_answering_is_enabled? && permission_action.subject == :question_answer && permission_action.action == :create
         end
 
         # Whether the user can manage the given process or not.
@@ -153,12 +141,6 @@ module Decidim
           @question ||= context.fetch(:question, nil)
         end
 
-        def has_access?
-          toggle_allow(admin_user? ||
-                           can_manage_process?(role: :committee) ||
-                           can_manage_process?(role: :service))
-        end
-
         def admin_creation_is_enabled?
           current_settings.try(:creation_enabled?) &&
               component_settings.try(:official_questions_enabled)
@@ -169,12 +151,12 @@ module Decidim
               component_settings.try(:question_answering_enabled)
         end
 
-        def create_permission_action?
-          permission_action.action == :create
-        end
-
         def participatory_texts_are_enabled?
           component_settings.participatory_texts_enabled?
+        end
+
+        def is_recipient?
+          question.try(:recipients).present? && question.try(:recipients).include?(user)
         end
       end
     end
