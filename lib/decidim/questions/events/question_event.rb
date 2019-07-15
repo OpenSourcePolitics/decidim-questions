64d798688b44e516a247686f17064c648355d299
moustachu <git@moustachu.net>
2019-07-15 21:07:59 +0200

    :bug: :globe_with_meridians: i18n interpolations

--- lib/decidim/questions/events/question_event.rb
+++ lib/decidim/questions/events/question_event.rb
@@ -7,10 +7,9 @@ module Decidim
         extend ActiveSupport::Concern
 
         included do
-          i18n_attributes :question_title, :question_body, :question_answer
+          i18n_attributes :question_body, :question_answer
           i18n_attributes :question_reference, :question_short_ref
-          i18n_attributes :question_url, :question_admin_edit_url
-          i18n_attributes :question_admin_answer_url, :question_admin_privates_notes_url
+          i18n_attributes :question_admin_edit_url, :question_admin_answer_url, :question_admin_privates_notes_url
 
           def question_presenter
             return unless resource
@@ -36,14 +35,6 @@ module Decidim
 
           def question_body
             resource_text
-          end
-
-          def question_title
-            resource_title
-          end
-
-          def question_url
-            resource_url
           end
 
           def question_admin_edit_url
